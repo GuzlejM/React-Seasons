6f215e21959444aa86cf151491aaf15a2f071fd8
Martin <martin.guzlej@gmail.com>
2020-09-30 11:57:42 +0100

    Commented code so its readable in the future

--- src/index.js
+++ src/index.js
@@ -6,27 +6,33 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        //THIS IS THE ONLY TIME we do direct assignment
+        // Making OBJECT called STATE with assinged properties lat: , errorMessage:
+        // THIS IS THE ONLY TIME we do direct assignment
         this.state = { lat: null, errorMessage: '' };
     }
 
     componentDidMount() {
+        // Using built-in function windown.navigator.geolocation.getCurrentPosition()
+        // to get location of the user. Asigning two callbacks positon and error 
+        // which are changing states of properties of STATE OBJECT from ABOVE
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errorMessage: err.message })
         );
     }
 
-    // componentDidUpdate() {
-
-    // }
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered!')
+    }
     // React says we have to define render !!
+
+    // Rendering by Statemens of class App
     render() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>
+            return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay />
+            return <SeasonDisplay  lat={this.state.lat}/>
         }
         return <div>Loading!</div>
     }
